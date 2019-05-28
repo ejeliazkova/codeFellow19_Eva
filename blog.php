c281f1b7003e9e2bbe1ad6a08f4a07fc2ecfe7fd
Eva Jeliazkova <eva@wustl.edu>
2019-05-28 14:26:16 -0500

    comitbeforepractice

--- blog.php
+++ blog.php
@@ -1,27 +1,17 @@
 <?php
 
-$posts = [
-    [
-     'Title' => 'Resume',
-     <evaResume class="doc"></evaResume>
-    ],
-    [
-      'Name' => 'Bracken',
-      'PhoneNumber' => '555-555-5555'
-    ],
-    [
-      'Name' => 'Michael',
-      'PhoneNumber' => '101-001-10001'
-    ],
-    
-  ];
+$blogPostId = 1;
+$allPosts = [
+    '1' => [],
+    '2' => [],
+    '3' => [],
+    '4' => []
+];
 $count = 1;
 
 foreach($posts as $individualPost){
     echo "
-        <div style='font-size:12px; color:#999;'>Post #".($count)."</div>
-        <div style='font-size:16px; font-weight:bold;'>".$individualPost['Title']."</div>
-        <div style='font-size:14px; margin-bottom:20px;'>".$individualPost['PhoneNumber']."</div>
+        <h2>".allPosts[blogPostId][title]."
     ";
     $count = $count + 1;
 }
