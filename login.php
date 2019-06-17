b407003f50356f4782b9dc2d2184cc7379b1e321
Eva Jeliazkova <eva@wustl.edu>
2019-06-17 16:19:30 -0500

    update

---
+++ login.php
@@ -0,0 +1,15 @@
+<?php
+include('config/init.php');
+echo"Login please";
+?>
+
+<form action='' method = 'post'>
+    <p class= 'Login'>
+        username: <input type='text' name='username' /><br/>
+        password: <input type='text' name='password' /><br />
+    <br/><br/>
+    <!-- submit button-->
+    <button onclick= "location.href='viewPerson.php'"> Enter</button>
+</div>
+    />
+</form>
--- viewPerson.php
+++ viewPerson.php
@@ -9,9 +9,25 @@ $theJob = getSpecificJobs($person['personId']);
 
 echoTaskManager($person);
  
+echo"these are my options:";
+
 foreach($theJob as $oneJob){
-    echo"these are my options:" .$oneJob['company']." as a ".$oneJob['position'];
+    echo" " .$oneJob['company']." as a ".$oneJob['position'];
 }
+?>
+<form action='' method = 'post'>
+    <p class= 'Login'>
+    Next Step: <select name='next step'>
+        <option value='default'>- -</option>
+        <option value='Job Title'> Job Title</option>
+        <option value='Information'> Information</option> 
+        <option value='Documents'>Documents</option>
+        <option value='Status'>Status</option> 
+    </select>
+    <br/><br/>
+    <!-- submit button-->
+    <input type='submit' name='add' value='add' />
+</form>
 
    
 
