431490d0db3e75ba4fbd9c709cc8a565cffbd27f
Eva Jeliazkova <eva@wustl.edu>
2019-06-18 11:48:17 -0500

    merging branches together

--- hello.php
+++ hello.php
@@ -8,6 +8,34 @@
 
 <img src= 'homePic.jpg' class= 'responsiveImage'/>
 
+<div class= 'BlockOne'>
+        <div class= 'aboutParent'>  
+            <div class= 'child_1'>    
+                <h2>Basics</h2>    
+                    <p>Hi :) My name is Eva and I'm a St. Louis native. I'm double majoring in Cognitive Neuroscience
+                and Spanish at WashU. I've always been curious about language and language aquisition which is
+                most likely inspired by my bilingual upbringing. While I spoke English at school, I mainly spoke
+                Bulgarian at home. 
+                    </p>
+
+                    <p>As a kid, I sometimes struggled to switch back and forth between the two languages: often
+                forgetting a word or hesitating to speak. It was pretty frustrating and confusing, but overall
+                empowering. I could communicate with an entirely different demographic in way that picked up on the
+                nuances of the culture. This duality is increasingly commonplace in today's globally connected
+                world so I think a lot of people can relate.
+                    </p>
+
+                    <p>My personal experience and the understanding that this is not unique occurrence sparked an interest in
+                the neurological basis and mental processes behind language learning and usage. How do you learn and 
+                remember all of the words and rules of a new language? And how does the answer to that question factor
+                into our increasingly interconnected world? And finally, how can we create and improve our technology
+                to aid in language aquisition, communication, and understanding across a global network?
+                    </p>
+            </div>
+        </div>
+        <div class='logo'></div>
+    </div>
+
     <div class='pageWrapper1'>
         <div class="homeParent">
             <div class="child_1">
@@ -33,20 +61,30 @@
         
     </div>
 
-    <div class='pageWrapper2'>
-       <div class= 'homeParent'> 
-        <h2>Some thoughts on some specific things</h2>
-        <p>College is almost over and, for me, that means it's time to stress about post-grad lyfe.</p>
-        <a class='funLink' href="babySteps.html">Post Grad Action Steps</a>
-        <p>Fortunately, I've also had time to explore some specific interests...</p>
-        <a class='funLink' href="art.html">Mexican Art</a>
+    <div class='pageWrapper1'>
+        <div class="projectParent"> 
+            <div class="child_1">
+                <h2>BioSURF</h2>
+                <p>Washington University Biology Summer '18 <br> Undergraduate Research Fellowship Program</p>
+                <a href="tracing.php">
+                    <div class="companyLogo" alt="HTML tutorial"></div>
+                </a>
+            </div>    
+            <div class="child_2">
+                <h2>LACRM</h2>
+                <p>Less Annoying CRM Summer '19 Coding<br> Fellowship Program in St. Louis, MO</p>
+                <a href="fellowship.php"> 
+                    <div class="companyLogo" alt="HTML tutorial"></div>
+                </a>
+            </div>
+        
         </div>
     </div>
 
     <div class='pageWrapper3'>
         
         <h3>You can find me on...</h3>
-        <table>
+        <table class = 'social'>
             <tr>
                 <td><a href="https://www.linkedin.com/in/eva-j-33b5b2bb/">linkedin</a></td>
                 <td><a href="http://www.marrowad.com/about.html">research</a></td>
